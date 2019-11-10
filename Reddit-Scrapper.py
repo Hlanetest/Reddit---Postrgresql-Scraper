7883a0cfc487f2e078aef23681d4f2e4a45baf90
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2019-11-09 19:49:46 -0500

    Update Reddit-Scrapper.py

--- Reddit-Scrapper.py
+++ Reddit-Scrapper.py
@@ -1,33 +1,40 @@
-#All the neccesary imports
 import praw
 import json
 import pprint
-import RedditCreds
 import sys
 import psycopg2
-reddit = RedditCreds.reddit_login()
 
-class postings:
-    def __init__(self, sub_title, sub_id, sub_score, sub_url, author)
-    def scrapper():
-        subreddit = reddit.subreddit('politics')
-        for submission in reddit.subreddit('politics').new(limit=10):
-            #stores all the findings into various variables
-            sub_title = submission.title
-            sub_id = submission.id
-            sub_score = submission.score
-            sub_url = submission.url
-            author = submission.author
-            
 
-    def inserter():
-        conn = psycopg2.connect("dbname=root user=postgres")
+
+
+class PsqlConnector():
+    def __init__(self, hostname, dbname, username, password):
+        '''handles the connection'''
+        self.hostname = hostname
+        self.dbname = dbname
+        self.username = username
+        self.password = password
+
+    def connect(self):
+        conn = psycopg2.connect(
+            hostname = self.hostname,
+            port = 5342, 
+            dbname = self.dbname, 
+            user = self.username, 
+            password = self.password)
+        
+
+    def insert(self, array):
         cur = conn.cursor()
-        cur.execute("select * from submissions;")
+        cur.execute("insert into submissions ;")
+
+hostname = '127.0.0.1'
+dbname = 'root'
+username = 'man'
+password = 'stuff'
 
 
 
-
-
-
-
+psqlconn = PsqlConnector(hostname, dbname, username, password)
+psqlconn.connect()
+psqlconn.insert(array)
