2b123e850fb2584628aa7ce87fde834a4043a1c4
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2019-11-14 16:30:10 -0500

    Update postgresql_connection.py

--- postgresql_connection.py
+++ postgresql_connection.py
@@ -3,41 +3,6 @@ import json
 import pprint
 import sys
 import psycopg2
-
-
-class PsqlConnector():
-    def __init__(self, dbname, username, password):
-        '''handles the connection'''
-        self.dbname = dbname
-        self.username = username
-        self.password = password
-
-    def connect(self):
-        '''handles the variables for the creds'''
-        conn = psycopg2.connect(
-            #port = 5342, 
-            dbname = self.dbname, 
-            user = self.username, 
-            password = self.password)
-        
-
-    def insert():
-        '''where we pass the various inserts and select statments'''
-        cur = conn.cursor()
-        cur.execute("select * from submissions;")
-        print ("I worked!")
-
-with open ("/home/hadrian/Documents/psqlcreds.json") as creds:
-    '''where we pull the various creds from.'''
-    data = json.load(creds)
-    dbname = data["dbname"]
-    username = data["username"]
-    password = data["password"]
-import praw
-import json
-import pprint
-import sys
-import psycopg2
 import Scrapper
 scrapper = Scrapper.reddit_scrapper()
 
