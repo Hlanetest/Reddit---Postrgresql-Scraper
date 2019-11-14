11203742e1ce572cb47d1fc061690e72b17ada1d
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2019-11-14 00:14:31 -0500

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import RedditCreds
 
 reddit = RedditCreds.reddit_login()
 
-def scrapper():
+def reddit_scrapper():
     subreddit = reddit.subreddit('politics')
     findings = []
     for submission in reddit.subreddit('politics').new(limit=10):
@@ -12,5 +12,6 @@ def scrapper():
         sub_score = submission.score
         sub_url = submission.url
         author = submission.author
+        return(sub_title, sub_id, sub_score, sub_url, author)
     
-scrapper()
+reddit_scrapper()
