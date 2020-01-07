77ceb5bc0828a8994c13487dadcfdc5a03941899
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2020-01-07 17:19:15 -0500

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,17 +1,35 @@
 import praw
 import RedditCreds
+import PsqlConnection
 
 reddit = RedditCreds.reddit_login()
 
+f = PsqlConnection.PsqlConnector('root', 'postgres')
+conn = f.connect()
+cur = conn.cursor()
+
+
+    
 def reddit_scrapper():
     subreddit = reddit.subreddit('politics')
-    findings = []
-    for submission in reddit.subreddit('politics').new(limit=10):
+    for submission in reddit.subreddit('politics').top(limit=120):
         sub_title = submission.title
         sub_id = submission.id
         sub_score = submission.score
         sub_url = submission.url
         author = submission.author
-        return(sub_title, sub_id, sub_score, sub_url, author)
-    
+        cur.execute("select * from submissions where submission_id = '{}';".format(sub_id))
+        a = cur.fetchone()
+        if a is None:
+            print (sub_title)
+            cur.execute("insert into submissions (submission_title, submission_id, submission_score, submission_url, submission_author) values ({}, '{}', '{}', '{}', '{}');".format(sub_title, sub_id, sub_score, sub_url, author).replace(':', ''))
+            print(sub_title)
+            print(sub_url)
+            conn.commit()
+            print("I worked")
+        else:
+            print (sub_title)
+            print("Object already exists")
+
+
 reddit_scrapper()
