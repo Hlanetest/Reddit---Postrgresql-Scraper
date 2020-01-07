43f9707639ff10763e971f8fab0ac6e546c4ff50
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2020-01-07 17:21:02 -0500

    Update Create_submissions_table.sql

--- Create_submissions_table.sql
+++ Create_submissions_table.sql
@@ -1,8 +1,8 @@
 create table submissions (
 	id serial primary key,
-	submission_title char(40),
+	submission_title text not null,
 	submission_id numeric (10,2),
 	submission_score numeric(10,2),
-	submission_url text[],
-	submission_autor text[]
+	submission_url text not null,
+	submission_author text not null
 	);
